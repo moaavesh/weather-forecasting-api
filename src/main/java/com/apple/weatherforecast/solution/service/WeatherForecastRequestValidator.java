dabf8381617cc2a050b9ad0951866c4e14f55f96
Mohammad Aavesh <moaavesh@gmail.com>
2024-03-07 12:09:08 +0530

    zipcode validator

--- src/main/java/com/apple/weatherforecast/solution/service/WeatherForecastRequestValidator.java
+++ src/main/java/com/apple/weatherforecast/solution/service/WeatherForecastRequestValidator.java
@@ -10,6 +10,7 @@ public class WeatherForecastRequestValidator {
     public  void validate(WeatherForecastRequest request){
         String zipCode = request.getZipCode();
         if(zipCode == null || zipCode.isEmpty()) throw new WeatherForecastServiceException("Zipcode is empty");
-        if(zipCode.length()<6) throw new WeatherForecastServiceException("Zipcode entered is incorrect");
+        // USA has 5 digits,  IND has 6 digits
+        if(zipCode.length()<4 ) throw new WeatherForecastServiceException("Zipcode entered is incorrect");
     }
 }
